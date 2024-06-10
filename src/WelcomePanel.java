d49e43dbcbd74963d6398957c20f2c3e456ebdab
prajwolp@nycstudents.net <PrajwolPandey>
2024-06-09 20:51:07 -0400

    adds images to menu

--- src/WelcomePanel.java
+++ src/WelcomePanel.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 
 public class WelcomePanel extends JPanel implements ActionListener {
     private BufferedImage background;
-
+    private BufferedImage chunLi;
+    private BufferedImage Ryu;
     private JTextField playerOneName;
     private JTextField playerTwoName;
     private JButton submitButton;
@@ -23,6 +24,19 @@ public class WelcomePanel extends JPanel implements ActionListener {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+
+        try {
+            chunLi = ImageIO.read(new File("src/ChunLi.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Ryu = ImageIO.read(new File("src/Ryu.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
         enclosingFrame = frame;
         playerOneName = new JTextField(10);
         playerTwoName = new JTextField(10);
@@ -35,18 +49,19 @@ public class WelcomePanel extends JPanel implements ActionListener {
 
     @Override
     public void paintComponent(Graphics g) {
-
         super.paintComponent(g);
         g.drawImage(background, 0, 0, null);
         g.setFont(new Font("Arial", Font.BOLD, 50));
         g.setColor(Color.WHITE);
         g.drawString("Please enter your name's :", 400, 100);
         g.setFont(new Font("Arial", Font.BOLD, 15));
-        g.setColor(Color.BLUE);
+        g.setColor(Color.BLACK);
         g.drawString("NAME ONE:", 615, 250);
         playerOneName.setLocation(600, 280);
-        g.setColor(Color.RED);
+        g.drawImage(chunLi, 400, 200, null);
+        g.setColor(Color.BLACK);
         g.drawString("NAME TWO:", 615, 320);
+        g.drawImage(Ryu, 800, 300, null);
         playerTwoName.setLocation(600, 350);
         submitButton.setLocation(615, 400);
     }
